9d3f023471cdfcc093d48c3a00a5a05ee7fa846f
Sitois <143238636+Sitois@users.noreply.github.com>
2023-12-01 17:18:25 +0100

    Create fr_en_commands.py

---
+++ fr_en_commands.py
@@ -0,0 +1,64 @@
+info_channel = {
+    "fr": "ID du Salon :",
+    "en": "Channel ID :"
+}
+
+info_nitro = {
+    "fr": "ID du Salon où les Nitro seront envoyés :",
+    "en": "Channel ID where the Nitro will be sent  :"
+}
+
+cat_error = {
+    "fr": "Aucune image de chat disponible.",
+    "en": "No cat pictures available."
+}
+
+clear_fail = {
+    "fr": "❌ | Quantité Incorrect, Max:",
+    "en": "❌ | Incorrect Number, Max:"
+}
+
+request_not_success = {
+    "fr": "Une erreur s'est produite lors de l'envoi de la requête :",
+    "en": "An error has occured :"
+}
+
+deleted_messages = {
+    "fr": "messages supprimés.",
+    "en": "messages has been deleted."
+}
+
+account_id_info = {
+    "fr": "ID du compte :",
+    "en": "Account ID :"
+}
+
+ai_for_all_boo = {
+    "fr": "AI_Pour_Tous",
+    "en": "AI_For_All"
+}
+
+ai_command_help = {
+    "fr": "Commandes IA ",
+    "en": "AI Command "
+}
+
+true_bool = {
+    "fr": "Oui",
+    "en": "Yes"
+}
+
+false_bool = {
+    "fr": "Non",
+    "en": "No"
+}
+
+clear_ai_help = {
+    "fr": "Supprimer toutes les conversations IA",
+    "en": "Clear all AI conversations"
+}
+
+part_two_help = {
+    "fr": "COMMANDES PERSONNALISÉES",
+    "en": "CUSTOM COMMANDS"
+}

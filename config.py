6068658d3ffb20fc2bc617b0d162fb17a9533dcb
Sitois <143238636+Sitois@users.noreply.github.com>
2023-12-01 17:17:50 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+"""CONFIG GENERAL"""
+disclaimer = "yes" # yes / no  |  Affichera le disclaimer.
+# Entrez votre token humain Discord (vous aurez seulement à appuyer sur entrée lors de la demande de token si vous le renseignez ici).
+token = ""
+langue = "en" # fr / en
+# Entrez ici votre chemin d'accès vers le le fichier main.py
+main_path = ""

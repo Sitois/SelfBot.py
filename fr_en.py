1f42b3f3afbc8a4609e422632b8c405bf1f38f4e
Sitois <143238636+Sitois@users.noreply.github.com>
2023-12-01 17:19:01 +0100

    Create fr_en.py

---
+++ fr_en.py
@@ -0,0 +1,159 @@
+warning_message = {
+    "fr": "ATTENTION : Les SelfBots NE SONT PAS AUTORISEE PAR DISCORD et peuvent facilement bannir votre compte. Merci de l'utiliser seulement pour des raisons éducatives.",
+    "en": "WARNING : SelfBots ARE NOT AUTHORIZED / ALLOWED BY DISCORD and can easily ban your account. Please use for educational purposes only."
+}
+
+
+login_token_message = {
+    "fr": "Entrez ici un token HUMAIN :",
+    "en": "Enter an human token here :"
+}
+
+
+error_token = {
+    "fr": "Il semble qu'aucun token n'est été fourni...",
+    "en": "It seems like no token has been given..."
+}
+
+error_choice = {
+    "fr": "Choix Incorrect.",
+    "en": "Incorrect Choice."
+}
+choix_ecriture = {
+    "fr": "Pour simuler l'écriture dans un salon.",
+    "en": "To simulate writing in a channel."
+}
+
+choix_envoyer = {
+    "fr": "Pour envoyer un message dans un salon.",
+    "en": "To send a message in a channel."
+}
+
+choix_quit = {
+    "fr": "Quitter le script.",
+    "en": "Exit script."
+}
+
+choix_customiser_statut = {
+    "fr": "Customiser son statut.",
+    "en": "To customise your status."
+}
+
+choix_change_status = {
+    "fr": "Changer de statut.",
+    "en": "To change your status."
+}
+
+choix_fake_nitro = {
+    "fr": "Envoie un FakeNitro dans le salon fourni.",
+    "en": "Send a FakeNitro."
+}
+
+choix_command = {
+    "fr": "Démarre la partie commande du SelfBot. (voir config_commands.py)",
+    "en": "Start the command part of the SelfBot. (see config_commands.py)"
+}
+
+barriere_un = {
+    "fr": "  |  ",
+    "en": "  |  "
+}
+
+barriere_deux = {
+    "fr": "                   |  ",
+    "en": "                    |  "
+}
+
+barriere_trois = {
+    "fr": "                      |  ",
+    "en": "             |  "
+}
+
+choix_des_options = {
+    "fr": "Votre choix :",
+    "en": "Select :"
+}
+
+
+input_channel = {
+    "fr": "ID du salon :",
+    "en": "Channel ID :"
+}
+
+request_success = {
+    "fr": "La requête a été envoyée avec succès.",
+    "en": "Request has been sent succesfully."
+}
+
+request_not_success = {
+    "fr": "Une erreur s'est produite lors de l'envoi de la requête :",
+    "en": "An error has occured :"
+}
+
+exit_ask = {
+    "fr": "Appuyez sur Entrée pour sortir.",
+    "en": "Press Enter to exit."
+}
+
+exit_ask_warning = {
+    "fr": "Appuyez sur Entrée pour continuer.",
+    "en": "Press Enter to continue."
+}
+
+
+message_content = {
+    "fr": "Contenu du message :",
+    "en": "Message content :"
+}
+
+new_status_ask = {
+    "fr": "Nouveau statut :",
+    "en": "New status :"
+}
+
+status_success_part_un = {
+    "fr":"Le statut a été changé en '",
+    "en": "The status got changed to '"
+}
+status_success_part_deux = {
+    "fr": "' avec succès !",
+    "en": "' with success !"
+}
+
+status_fail_part_un = {
+    "fr": "",
+    "en": ""
+}
+
+status_fail_part_deux = {
+    "fr": "Message d'erreur :",
+    "en": "Error message :"
+}
+
+
+status_ask = {
+    "fr": "Entrez le statut que vous souhaitez définir (online, idle, dnd, invisible) : ",
+    "en": "Enter the status (online, idle, dnd, invisible) : "
+}
+
+status_set_part_un = {
+    "fr": 'Statut changé en "',
+    "en": 'Status set to "'
+}
+
+status_set_part_deux = {
+    "fr": '" avec succès !',
+    "en": '" successfully !'
+}
+
+
+
+command_success = {
+   "fr": "Partie commande démarrer avec succès.",
+   "en": "Command part loaded."
+}
+
+command_wait = {
+   "fr": "En l'attente de commande...",
+   "en": "Waiting for commands..."
+}

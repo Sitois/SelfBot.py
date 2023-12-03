70c2a05ebd447714bfb0a90861ee5d66396315e0
Sitois <143238636+Sitois@users.noreply.github.com>
2023-12-03 14:05:59 +0100

    Update config_commands.py

--- config_commands.py
+++ config_commands.py
@@ -1,17 +1,19 @@
 debug_mode = False # (beta)
 """CONFIG  GENERAL COMMANDES"""
 # Token du compte pour les commandes.
-commands_token = "YOUR TOKEN"
+commands_token = ""
 # Votre ID du compte qui éxécutera les commandes.
-account_id = ["YOUR ACCOUNT ID"]
+account_id = [""]
 # ID du salon par défaut (vous pourrez ajouter d'autre salon en fesant: la commande en-dessous et l'id du salon. )
-CHANNEL_ID = "DEFAULT CHANNEL ID" # Laisser vide pour avoir la demande au démarrage.
+CHANNEL_ID = "" # Laisser vide pour avoir la demande au démarrage.
 add_channel = "add"
 del_channel = "del"
+list_channel = "list"
 # Prefix des commandes
 prefix = "-"
 #
 commande_aled = "danger"
+
 
 """CONFIG COMMANDES"""
 # fr: Nom de la commande QUI ENVERRA LE MESSAGE D'AIDE  (fonctionne seulement avec votre ID)
@@ -22,7 +24,7 @@ help_definition = "Send the help of the SelfBot."
 
 #
 eval_command = "eval"
-#
+# DO NOT ENTER "True" unless you are aware of the risks (eval command allows you to execute arbitrary code)
 eval_toggle = False
 
 # ai
@@ -30,21 +32,23 @@ commande_ai = "ai"
 #
 commande_ia_clear = "suppr"
 # fr: Si vous possédez Nitro entrez ici 3900, si vous ne possédez pas nitro, entrez ici 1900.
-is_nitro = 1900
+is_nitro = 3900
 # fr: Identifiants et mot de passe pour la commande -ai.
-hug_chat_email = "YOUR EMAIL"
-hug_chat_password = "YOUR PASSWD"
+hug_chat_email = ""
+hug_chat_password = ""
 #
 ai_for_all = False # True / False
+#
+language_ai = "english"
 
 # fr: Activé NitroSniper (True = oui et False = non)
-# en:
-nitro_toggle = True # True / False
-# fr: Salon où le Nitro sera envoyé.
-# en: Channel ID for the Nitro Sniper.
-notifier_channel_id = "CHANNEL ID"
+# en: Enable NitroSniper (True = yes and False = no)
+nitro_toggle = False # True / False
+# fr: Salon où le Nitro sera envoyé. (il est recommendé d'entrer un salon privé (mp / dm))
+# en: Channel ID for the Nitro Sniper. (recommended: an dm channel id.)
+notifier_channel_id = ""
 # Notifier Account
-notifier_token = "YOUR SECOND ACCOUNT TOKEN"
+notifier_token = ""
 
 
 # fr: Nom de la commande qui servira a clear ses messages. (fonctionne seulement avec votre ID)
@@ -71,21 +75,21 @@ nitro_definition = "Send a FakeNitro"
 # Nom de la quatrième commande (préféré pour raccourcir la commande d'un snipe), (fonctionne seulement avec votre ID)
 snipe_command = "snipe"
 # Nom de votre commande de snipe.
-snipe_answer = "!snipe"
+snipe_answer = "~iesnipe"
 # Description de la commande
 snipe_definition = "Snipe."
 
 
 
 """fr: COMMANDES PERZSONNALISéES
-   en: PERSONNALIZED COMMANDS"""
+   en: CUSTOM COMMANDS"""
 
 # Nom de la commande (fonctionne seulement avec votre ID)
-commande_un = "COMAND NAME"
+commande_un = ""
 # Réponse de la troisième commande
-reponse_un = "COMMAND ANSWER"
+reponse_un = ""
 # fr: Définition de la commande
-un_definition ="COMMAND DEFINITION"
+un_definition =""
 
 # Nom de la deuxième commande (fonctionne seulement avec votre ID)
 commande_deux = ""
@@ -98,8 +102,8 @@ deux_definition =""
 commande_trois = ""
 # Réponse de la cinquième commande
 reponse_trois = ""
-#
-trois_definition = ""
+# Definition
+trois_definition =""
 
 # Nom de la quatrième commande (fonctionne seulement avec votre ID)
 commande_quatre = ""

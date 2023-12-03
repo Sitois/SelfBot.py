2887f8ea67cf233bca55f78a55a40dd9d4df6a09
Sitois <143238636+Sitois@users.noreply.github.com>
2023-12-03 14:05:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 """CONFIG GENERAL"""
-disclaimer = "yes" # yes / no  |  Affichera le disclaimer.
-# Entrez votre token humain Discord (vous aurez seulement à appuyer sur entrée lors de la demande de token si vous le renseignez ici).
+disclaimer = "yes" # yes / no  |  Display the disclaimer. / Affichera le disclaimer.
+# fr: Entrez votre token humain Discord (vous aurez seulement à appuyer sur entrée lors de la demande de token si vous le renseignez ici).
+# en: Enter your human token. (you'll only have to press enter when requesting a token if you enter it here).
 token = ""
 langue = "en" # fr / en
-# Entrez ici votre chemin d'accès vers le le fichier main.py
+# fr: Entrez ici votre chemin d'accès vers le le fichier main.py
+# en: Enter the path to main.py
 main_path = ""
